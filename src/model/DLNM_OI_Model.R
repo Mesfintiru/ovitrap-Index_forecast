8cc20789170cc6ef379a2154b0b2f3661963fa78
Mesfintiru <44431294+Mesfintiru@users.noreply.github.com>
2019-08-06 17:10:49 +0800

    Update DLNM_OI_Model.R

--- src/model/DLNM_OI_Model.R
+++ src/model/DLNM_OI_Model.R
@@ -1,31 +1,46 @@
 # clean the environment
+
 rm(list=ls(all=TRUE))
 
+
 # select an area between: NTS, NTN, KL, HK 
+
 area <- "KL"
 
+
 # load from csv file into 'data' variable
+
 filePath <- paste("data/monthly_data_", area, ".csv", sep="") 
+
 data <- read.csv(filePath, header=T)
 
+
 # clean data (esp. those with '#')
+
 for (fieldName in names(data)) {
-  data[[fieldName]] <- as.numeric(gsub("[^.0-9]", "", data[[fieldName]]))
+  
+  data[[fieldName]] <- as.numeric(gsub("[^.0-9]", "", data[[fieldName]])) 
+  
 }
 
 
 library("dlnm")
+
 library("splines")
 
-cb.meanTemperature <- crossbasis(data$temperature_avg, lag=4, 
+
+# create crossbasis variables
+
+
+cb.meanTemperature <- crossbasis(data$temperature_avg, lag=3, 
                                  argvar=list(fun="ns", df=3),
-                                 arglag=list(fun="ns", knots=logknots(4, 1)))
-cb.totalRainfall <- crossbasis(data$total_rain, lag=2,
+                                 arglag=list(fun="ns", knots=logknots(3, 1)))
+cb.totalRainfall <- crossbasis(data$total_rain, lag=6,
                                argvar=list(fun="ns", df=3),
-                               arglag=list(fun="ns", knots=logknots(2, 1)))
-     
-model <- glm(ovitrap_idx ~ cb.meanTemperature + cb.totalRainfall , family=gaussian(), data)     
+                               arglag=list(fun="ns", knots=logknots(6, 1)))
+
+
+model <- glm(TPC ~ cb.meanTemperature + cb.totalRainfall , family=poisson,data)     
+
 summary(model)
-
-
 
