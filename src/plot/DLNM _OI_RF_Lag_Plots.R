87bfa78056d5adeb6b6d29dc4b8cf471e5be7866
Mesfintiru <44431294+Mesfintiru@users.noreply.github.com>
2019-08-06 17:05:56 +0800

    Update DLNM _OI_RF_Lag_Plots.R

--- src/plot/DLNM _OI_RF_Lag_Plots.R
+++ src/plot/DLNM _OI_RF_Lag_Plots.R
@@ -1,42 +1,34 @@
-# clean the environment
-rm(list=ls(all=TRUE))
-
-# select an area between: NTS, NTN, KL, HK 
-area <- "KL"
-
-# load from csv file into 'data' variable
-filePath <- paste("data/monthly_data_", area, ".csv", sep="") 
-data <- read.csv(filePath, header=T)
-
-# clean data (esp. those with '#')
-for (fieldName in names(data)) {
-  data[[fieldName]] <- as.numeric(gsub("[^.0-9]", "", data[[fieldName]]))
-}
-
-
-library("dlnm")
-library("splines")
-
-# create crossbasis variables
-cb.meanTemperature <- crossbasis(data$temperature_avg, lag=6, 
+cb.meanTemperature <- crossbasis(data$temperature_avg, lag=3, 
                                  argvar=list(fun="ns", df=3),
-                                 arglag=list(fun="ns", knots=logknots(6, 1)))
+                                 arglag=list(fun="ns", knots=logknots(3, 1)))
 cb.totalRainfall <- crossbasis(data$total_rain, lag=6,
                                argvar=list(fun="ns", df=3),
                                arglag=list(fun="ns", knots=logknots(6, 1)))
 
-     
-model <- glm(ovitrap_idx ~ cb.meanTemperature + cb.totalRainfall , family=gaussian(), data)     
+
+model <- glm(TPC ~ cb.meanTemperature + cb.totalRainfall , family=poisson,data)     
+
 summary(model)
 
 
 
 # model prediction 
 
-pred.totalRainfall <- crosspred(cb.totalRainfall, model,bylag=1)
+pred.meanTemperature <- crosspred(cb.totalRainfall, model, by=0.5) 
 
-plot(pred.totalRainfall, xlab="Rainfall", zlab="RR", ylab="Lag") 
+# 3D Plot for lag effect of temperature 
 
-plot(pred.totalRainfall, "contour",
-     plot.title = title(xlab="Rainfall", ylab="Lag", main="Contour Graph"), 
-     key.title=title("RR")) 
+plot(pred.meanTemperature, xlab="Rainfall(mm)", zlab="RD", ylab="Lag")
+
+
+# Contour Plot for lag effect of rainfall 
+
+plot(pred.meanTemperature, "contour", 
+     
+     plot.title = title(xlab="Rainfall(mm)", ylab="Lag", main="Lag Effect of Monthly Total Rainfall on Mosquito Relative Density"), 
+     key.title=title("RD"))
+     
+     
+
+
+
