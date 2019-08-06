6260b25660270cafc06c4ae857dce9666f0d41f8
Mesfintiru <44431294+Mesfintiru@users.noreply.github.com>
2019-08-06 16:56:45 +0800

    Update INLA_DLNM_OI_Temp_Lag_plots.R

--- src/plot/INLA_DLNM_OI_Temp_Lag_plots.R
+++ src/plot/INLA_DLNM_OI_Temp_Lag_plots.R
@@ -1,12 +1,21 @@
 # clean the environment
+
 rm(list=ls(all=TRUE))
 
+
 # select an area between: NTS, NTN, KL, HK 
+
 area <- "KL"
 
+
+
 # load from csv file into 'data' variable
-filePath <- paste("data/monthly_data_", area, ".csv", sep="") 
+
+data <- paste("data/monthly_data_Poisson_", area, ".csv", sep="") 
+
 data <- read.csv(filePath, header=T)
+
+
 
 # clean data (esp. those with '#')
 for (fieldName in names(data)) {
@@ -16,14 +25,16 @@ for (fieldName in names(data)) {
 library("INLA")
 library("dlnm")
 library("splines")
+library(forecast)
 
-# create crossbasis variables
-cb.meanTemperature <- crossbasis(data$temperature_avg, lag=4, 
+# Creating cross basis for predictors 
+
+cb.meanTemperature <- crossbasis(data$temperature_avg, lag=3, 
                                  argvar=list(fun="ns", df=3),
-                                 arglag=list(fun="ns", knots=logknots(4, 1)))
-cb.totalRainfall <- crossbasis(data$total_rain, lag=2,
+                                 arglag=list(fun="ns", knots=logknots(3, 1)))
+cb.totalRainfall <- crossbasis(data$total_rain, lag=6,
                                argvar=list(fun="ns", df=3),
-                               arglag=list(fun="ns", knots=logknots(2, 1)))
+                               arglag=list(fun="ns", knots=logknots(6, 1)))
 
 getUpdatedColnames <- function(varName, oldColnames) {
   newColnames <- c()
@@ -48,9 +59,9 @@ colnames(cb.totalRainfall) <- getUpdatedColnames("cb.totalRainfall", colnames(cb
 
 # hyper.ar1 = list(theta1 = list(prior="pc.prec", param=c(1, 0.00001))) 
 
-formula1 <- ovitrap_idx ~  cb.meanTemperature + cb.totalRainfall + f(month, model="rw1") + f(year, model="iid") 
+formula1 <- TPC ~  cb.meanTemperature + cb.totalRainfall + f(month, model="rw1") + f(year, model="iid") 
 
-inla.fit <- inla(formula=formula1, data=data,
+inla.fit <- inla(formula=formula1, data=data,family = 'poisson',
               control.fixed=list(correlation.matrix=T),
               control.compute=list(dic=T, waic=T)) 
 # 
@@ -67,14 +78,24 @@ indcoef <- grep(cond, rownames(inla.fit$summary.fixed))
 inla.coef <- inla.coef[indcoef]
 inla.vcov <- inla.vcov[indvcov,indvcov,drop=FALSE]
 
+# Model prediction  
 
+pred.meanTemperature <- crosspred(cb.meanTemperature, by=1,  coef=inla.coef, vcov=inla.vcov, cumul=TRUE) 
 
-pred.meanTemperature <- crosspred(cb.meanTemperature, by=1,  coef=inla.coef, vcov=inla.vcov) 
+# Temperature Lag Effect 3D plot
 
-plot(pred.meanTemperature, xlab="Temperature", zlab="RR", ylab="Lag")
+plot(pred.meanTemperature, xlab="Temperature(°C)", zlab="RD", ylab="Lag")
+
+# Temperature Lag Effect Contour Plot 
 
 plot(pred.meanTemperature, "contour", 
-     plot.title = title(xlab="Temperature", ylab="Lag", main="Contour Graph"), 
-     key.title=title("RR"))
+     plot.title = title(xlab="  Temperature(°C)", ylab="Lag", main="Lag Effect of Monthly Average Tempertaure on Mosquito Relative Density"), 
+     key.title=title("RD"))
+
+
+# Plots for lag effect at very high and very low  Temperature 
+
+plot(pred.meanTemperature, "slices", var=28, col=3, ylab="RD", ci.arg=list(density=15,lwd=2), main=" Lag effect at 28°C Monthly Mean Temperature ") 
+plot(pred.meanTemperature, "slices", var=14, col=3, ylab="RD", ci.arg=list(density=15,lwd=2), main="Lag effect at 14°C Monthly Mean Temperature") 
 
 
